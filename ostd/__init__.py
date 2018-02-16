fb89bf8427aa15730f4a963a5183206b19e41aee
Philipp Ploder <philipp.ploder@outlook.com>
2018-02-16 17:59:51 +0100

    Extract entry to __main__.py file

--- ostd/__init__.py
+++ ostd/__init__.py
@@ -1,4 +1,3 @@
-import sys
 from xmlrpc.client import Transport, ServerProxy
 
 from ostd.utils import *
@@ -58,29 +57,3 @@ class Downloader:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: {} <USER-AGENT> <SUBTITLE-ID>'.format(sys.argv[0]), file=sys.stderr)
-        sys.exit(1)
-
-    USER_AGENT = sys.argv[1]
-    SUBTITLES_ID = sys.argv[2]
-
-    try:
-        with Downloader(USER_AGENT) as downloader:
-            result = next(downloader.download(SUBTITLES_ID))
-
-            if sys.platform == "win32":
-                import os
-                import msvcrt
-
-                msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
-
-            sys.stdout.buffer.write(result.raw)
-    except NotOKException as e:
-        status = e.response.get('status')
-        print('The download failed because the server response contained the following status: {}'.format(status),
-              file=sys.stderr)
-        sys.exit(1)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ostd',
-    version='1.0.0',
+    version='1.0.1',
 
     description='Downloads subtitles from OpenSubtitles',
     author='Philipp Ploder',
