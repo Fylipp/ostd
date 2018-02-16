814f7aa1012537f3c2eb2e608b95005161709e77
Philipp Ploder <philipp.ploder@outlook.com>
2018-02-16 16:46:20 +0100

    Extend setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from distutils.core import setup
 setup(
     name='ostd',
     version='1.0.0',
+
     description='Downloads subtitles from OpenSubtitles',
     author='Philipp Ploder',
     url='https://github.com/Fylipp/ostd',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='opensubtitles',
+
     py_modules=['ostd'],
-    install_requires=[
+    requires=[
         'chardet'
     ]
 )
